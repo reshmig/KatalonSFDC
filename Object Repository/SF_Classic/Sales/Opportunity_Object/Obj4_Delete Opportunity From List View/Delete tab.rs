cde860d7e501160866f80b92cdbdacdf325cb6bf
Shashank.Sathya <Shashank.Sathya@MLBBTL-106166.in.marlabs.com>
2021-01-13 21:03:32 +0530

    Committed by Shashank for Opportunity Object

---
+++ Object Repository/SF_Classic/Sales/Opportunity_Object/Obj4_Delete Opportunity From List View/Delete tab.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Delete tab</name>
+   <tag></tag>
+   <elementGuidId>f57e25ff-bc75-40b9-af82-14dd8b4750c1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//a//span[contains(text(),'Del')])[8]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/SF_Classic/Sales/Opportunity_Object/Obj4_Delete Opportunity From List View/Go.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Go</name>
+   <tag></tag>
+   <elementGuidId>f8e0f4b5-e439-4866-9bd8-13ac1cb661ec</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@value=' Go! ']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
